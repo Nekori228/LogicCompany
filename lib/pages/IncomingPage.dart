71958e432add4ac9d98eade61600ded297fadc76
Nekori228 <88834448+Nekori228@users.noreply.github.com>
2024-05-14 11:04:53 +0500

    Add 2 screen

--- lib/pages/IncomingPage.dart
+++ lib/pages/IncomingPage.dart
@@ -35,56 +35,244 @@ class _IncomingPageState extends State<IncomingPage> {
                 horizontal: MediaQuery.of(context).size.width * 0.05),
             child: Column(
               children: [
+                SizedBox(height: 30),
                 Container(
-                  height: 200,
+                  height: 160,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15.0),
                       color: Color(0xFF27262B)),
-                  child: Container(
-                    margin: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.03),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 20,
+                            width: 5,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFD9D9D9),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
                               '№ 20230 Новый',
                               style: TextStyle(
                                   color: Color(0xFFFFFFFF), fontSize: 20),
                             ),
-                            Spacer(),
-                            Text(
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                            child: Text(
                               'Связь ООО',
                               style: TextStyle(
                                   color: Color(0xFFFFFFFF), fontSize: 20),
                             ),
-                          ],
-                        ),
-                        Container(
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                        child: Container(
                           height: 1,
                           color: Color(0xFFD9D9D9).withOpacity(0.2),
                         ),
-                        Text(
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 0, 10, 0),
+                        child: Text(
                           'Сургут - Юганск',
                           style:
                               TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
                         ),
-                        Text(
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 8, 0, 0),
+                        child: Text(
                           '1 декабря 2023',
                           style:
                               TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
                         ),
-                        Align(
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        child: Align(
                           alignment: Alignment.topRight,
                           child: Text(
                             '120 000,00 Р',
                             style: TextStyle(
-                                color: Color(0xFFFFFFFF), fontSize: 20),
+                                color: Color(0xFFFFFFFF),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Color(0xFF27262B)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 20,
+                            width: 5,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFD9D9D9),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              '№ 20231 Новый',
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF), fontSize: 20),
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                            child: Text(
+                              'Нефть ООО',
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF), fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                        child: Container(
+                          height: 1,
+                          color: Color(0xFFD9D9D9).withOpacity(0.2),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 0, 10, 0),
+                        child: Text(
+                          'Сургут - Ханты-Мансийск',
+                          style:
+                          TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 8, 0, 0),
+                        child: Text(
+                          '1 декабря 2023',
+                          style:
+                          TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            '50 000,00 Р',
+                            style: TextStyle(
+                                color: Color(0xFFFFFFFF),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Color(0xFF27262B)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 20,
+                            width: 5,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFD9D9D9),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              '№ 20232 Новый',
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF), fontSize: 20),
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                            child: Text(
+                              'Югра ООО',
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF), fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                        child: Container(
+                          height: 1,
+                          color: Color(0xFFD9D9D9).withOpacity(0.2),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 0, 10, 0),
+                        child: Text(
+                          'Сургут - Ивдель',
+                          style:
+                          TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(17, 8, 0, 0),
+                        child: Text(
+                          '2 декабря 2023',
+                          style:
+                          TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            '200 000,00 Р',
+                            style: TextStyle(
+                                color: Color(0xFFFFFFFF),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 )
               ],
